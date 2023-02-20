75db7b50566ae6acf8160ae36ebaa839490e0bc8
Jihyun <75922558+jkim68888@users.noreply.github.com>
2023-02-20 14:40:34 +0900

    자바스크립트 통신 브릿지 완료 및 런치스크린 추가

--- hanaro/AppDelegate.swift
+++ hanaro/AppDelegate.swift
@@ -14,6 +14,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        sleep(2)
+        
         FirebaseApp.configure()
         
         // MARK: - Firebase FCM
@@ -32,18 +34,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
     }
     
     // MARK: - 토큰 갱신 모니터링
-    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
-      print("Firebase registration token: \(String(describing: fcmToken))")
-
-      let dataDict: [String: String] = ["token": fcmToken ?? ""]
-      NotificationCenter.default.post(
-        name: Notification.Name("FCMToken"),
-        object: nil,
-        userInfo: dataDict
-      )
-      // TODO: If necessary send token to application server.
-      // Note: This callback is fired at each app startup and whenever a new token is generated.
-    }
+//    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+//      print("Firebase registration token: \(String(describing: fcmToken))")
+//
+//      let dataDict: [String: String] = ["token": fcmToken ?? ""]
+//      NotificationCenter.default.post(
+//        name: Notification.Name("FCMToken"),
+//        object: nil,
+//        userInfo: dataDict
+//      )
+//      // TODO: If necessary send token to application server.
+//      // Note: This callback is fired at each app startup and whenever a new token is generated.
+//    }
 
     // MARK: UISceneSession Lifecycle
 
--- hanaro/ViewController.swift
+++ hanaro/ViewController.swift
@@ -10,19 +10,32 @@ import WebKit
 import FirebaseMessaging
 
 class ViewController: UIViewController {
-    @IBOutlet weak var webView: WKWebView!
+    var webView: WKWebView!
     
     // MARK: - 웹뷰 url
-    let dev = "http://dev.picaloca.com:3020/main"
+    let devSurvey = "http://dev.picaloca.com:3020/intro"
+    let devMain = "http://dev.picaloca.com:3020/main"
+    let testLogin = "http://dev.picaloca.com:3020/test_login"
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = UIColor(red: 243/255, green: 245/255, blue: 248/255, alpha: 1)
-        webViewInit()
+        setupWebView()
+        addSubviews()
+        makeConstants()
+        loadWebPage()
+        UserDefaults.standard.set(true, forKey: "isVisited")
     }
-
-    // MARK: - 웹뷰 초기화
-    func webViewInit() {
+    
+    func setupWebView() {
+        // Swift가 Javascript에게 setPushToken() 호출 요청
+        let contentController = WKUserContentController()
+        let configuration = WKWebViewConfiguration()
+        
+        contentController.add(self, name: "setPushToken")
+        
+        configuration.userContentController = contentController
+        webView = WKWebView(frame: .zero, configuration: configuration)
+        
         // 캐시 데이터는 앱 실행 시 제거
         WKWebsiteDataStore.default().removeData(ofTypes: [WKWebsiteDataTypeDiskCache, WKWebsiteDataTypeMemoryCache], modifiedSince: Date(timeIntervalSince1970: 0)) {
         }
@@ -30,21 +43,105 @@ class ViewController: UIViewController {
         // 좌 우 스와이프 동작시 뒤로 가기 앞으로 가기 기능 활성화
         webView.allowsBackForwardNavigationGestures = true
         
-        if let url = URL(string: dev) {
-            let request = URLRequest(url: url)
-            webView.load(request)
+        // delegate
+        webView.uiDelegate = self
+    }
+    
+    func addSubviews() {
+        view.addSubview(webView)
+    }
+    
+    func makeConstants() {
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        webView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        webView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        webView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        webView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+    }
+    
+    func loadWebPage() {
+        // URL 이동
+        if UserDefaults.standard.bool(forKey: "isVisited") {
+            if let url = URL(string: devMain) {
+                let request = URLRequest(url: url)
+                webView.load(request)
+            }
+        } else {
+            if let url = URL(string: devSurvey) {
+                let request = URLRequest(url: url)
+                webView.load(request)
+            }
         }
+        
+//        if let url = URL(string: testLogin) {
+//            let request = URLRequest(url: url)
+//            webView.load(request)
+//        }
     }
     
     // MARK: - FCM 토큰 세팅
-    func setPushToken() {
+    func setPushToken(completionHandler: @escaping (String) -> Void) {
         Messaging.messaging().token { token, error in
             if let error = error {
                 print("FCM 토큰 fetch 에러: \(error)")
             } else if let token = token {
                 print("FCM 토큰: \(token)")
-                
+                completionHandler(token)
             }
+        }
+    }
+}
+
+// MARK: - 자바스크립트 통신
+extension ViewController: WKScriptMessageHandler {
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        if message.name == "setPushToken" {
+            setPushToken() { [weak self] token in
+                guard let self = self else { return }
+                
+                let dict = [
+                    "token": token
+                ]
+                
+                let jsonData = try! JSONSerialization.data(withJSONObject: dict, options: [])
+                let jsonString = String(data: jsonData, encoding: .utf8)!
+                
+                self.webView.evaluateJavaScript("testCode(\(jsonString))") { result, error in
+                    guard error == nil else {
+                        print(error as Any)
+                        return
+                    }
+                }
+            }
+        }
+    }
+}
+
+// MARK: - 얼럿 extension
+extension ViewController: WKUIDelegate {
+    func webView(_ webView: WKWebView, runJavaScriptAlertPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping () -> Void) {
+        let alertController = UIAlertController(title: "", message: message, preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "확인", style: .cancel) { _ in
+            completionHandler()
+        }
+        alertController.addAction(cancelAction)
+        DispatchQueue.main.async {
+            self.present(alertController, animated: true, completion: nil)
+        }
+    }
+    
+    func webView(_ webView: WKWebView, runJavaScriptConfirmPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping (Bool) -> Void) {
+        let alertController = UIAlertController(title: "", message: message, preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "취소", style: .cancel) { _ in
+            completionHandler(false)
+        }
+        let okAction = UIAlertAction(title: "확인", style: .default) { _ in
+            completionHandler(true)
+        }
+        alertController.addAction(cancelAction)
+        alertController.addAction(okAction)
+        DispatchQueue.main.async {
+            self.present(alertController, animated: true, completion: nil)
         }
     }
 }
